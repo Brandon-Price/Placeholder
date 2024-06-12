b0ba5679f171ad851d5e211aaba30cd004a74b93
Brandon-Price <brandon.samuel.price@gmail.com>
2024-06-12 18:30:05 -0500

    Started API development

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,15 @@
 const express = require('express');
-const app = express;
+const app = express();
+const mongoose = require('mongoose');
+
+app.use(express.json());
+
+// Use http://127.0.0.1:8000/
+// localhost isn't working
+app.get('/api/posts', (req, res) => {
+    res.json('Hello World');
+});
+
+app.listen(process.env.PORT || 8000, () => {
+    console.log("Backend server is running");
+});
